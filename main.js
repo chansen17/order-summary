f55c67719420d3838ab2d47ae2c1c9feafe7455c
chansenhyggedev <chansen.hyggedev@gmail.com>
2023-08-10 20:03:32 -0700

    update

---
+++ main.js
@@ -0,0 +1,12 @@
+import './style.css'
+import 'animate.css';
+
+document.addEventListener('DOMContentLoaded', function () {
+  let card = document.querySelector('#card');
+  card.classList.add('animate', 'animate__animated', 'animate__fadeInDown');
+});
+
+
+
+
+
